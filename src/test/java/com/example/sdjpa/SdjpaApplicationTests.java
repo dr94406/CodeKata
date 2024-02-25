f8dbb8bba6bb8ba38b2c519613ff8ed935f27c54
min <dr94406@naver.com>
2024-02-25 15:20:01 +0900

    Initial commit

---
+++ src/test/java/com/example/sdjpa/SdjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
